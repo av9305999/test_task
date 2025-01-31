0731358d6381cedeb95776ff128be6a58a123d7c
Арина Власова <arina.vlasova.2001@bk.ru>
2025-02-01 01:37:13 +0700

    Create factory table

---
+++ src/factory/models.py
@@ -0,0 +1,13 @@
+from sqlalchemy.orm import Mapped, mapped_column, relationship
+
+from db.base import Base
+from db.tablenames import FACTORY_TABLE
+
+
+class Factory(Base):
+    __tablename__ = FACTORY_TABLE
+
+    sites: Mapped[list['Site']] = relationship(back_populates='factory')
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str]
