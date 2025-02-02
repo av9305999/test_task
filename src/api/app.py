a7f437a94f540e49dcaff2d2a77aa28a7adbee2a
Арина Власова <arina.vlasova.2001@bk.ru>
2025-02-03 01:55:02 +0700

    Update app

--- src/api/app.py
+++ src/api/app.py
@@ -5,17 +5,23 @@ from fastapi.responses import JSONResponse
 from starlette import status
 from starlette.middleware.cors import CORSMiddleware
 
+from api.v1.equipment import router as equipment_router
+from api.v1.factory import router as factory_router
+from api.v1.sites import router as sites_router
+from api.v1.setup import router as setup_router
+from api.v1.hierarchy import router as hierarchy_router
 from config import settings
+from db.exceptions import DoesNotExists
 
 
-async def on_not_found(request, exc):
+async def on_not_found(request: Request, exc: DoesNotExists):
     raise HTTPException(
         status_code=status.HTTP_404_NOT_FOUND,
-        detail="Not found."
+        detail=exc.message
     )
 
 
-async def on_validation_error(request, exc):
+async def on_validation_error(request: Request, exc: Exception):
     if settings.DEBUG:
         return JSONResponse(
             content=jsonable_encoder({"detail": exc.errors(), "body": exc.body}),
@@ -45,10 +51,14 @@ def init_app() -> FastAPI:
     )
 
     v1_router = APIRouter(prefix='/api/v1')
-    # v1_router.include_router(auth_router)
+    v1_router.include_router(factory_router)
+    v1_router.include_router(sites_router)
+    v1_router.include_router(equipment_router)
+    v1_router.include_router(hierarchy_router)
+    v1_router.include_router(setup_router)
 
     app.include_router(v1_router)
     app.exception_handler(RequestValidationError)(on_validation_error)
-    # app.exception_handler(DoesNotExists)(on_not_found)
+    app.exception_handler(DoesNotExists)(on_not_found)
 
     return app
